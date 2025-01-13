e6a291370f9dbcb2769c10296d067f6948a811a3
Shubahm <shubham.20205157@mnnit.ac.in>
2025-01-13 18:10:15 +0530

    New Deck Func Added

--- deck.go
+++ deck.go
@@ -4,6 +4,18 @@ import "fmt"
 
 type deck []string
 
+func newDec() deck{
+	cards := deck{}
+    cardSuits := deck {"Spades", "Diamonds","Hearts","Clubs"}
+	cardValues := deck {"Ace","Two","Three","Four"}
+	for _, suit := range cardSuits{
+		for _,value := range cardValues{
+			cards = append(cards, value + " of "+ suit)
+		}
+	}
+	return cards
+}
+
 func (cards deck) print() {
 	// this is reciever function
 	// every variable of type deck can use this function 
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 func main(){
-	cards := deck {"Ace of Diamond","Five of Spades"}
-
+	cards := newDec()
 	cards.print()
 }
